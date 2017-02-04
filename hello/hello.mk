c3d4b38402448fc97e9a737bf951873cab9695d8
Alessandro Power <alessandrojpower@gmail.com>
2017-02-04 20:16:56 +0000

    Added modes to Makefile.

--- hello/hello.mk
+++ hello/hello.mk
@@ -4,14 +4,20 @@ SRC_DIR = src
 
 CC = gcc
 CFLAGS = -std=c99 -Wall -Wextra -pedantic -Werror
-DEBUG_CFLAGS = -g
 RELEASE_CFLAGS = -O2 -s -DNDEBUG
+DEBUG_CFLAGS = -g
 
 .PHONY = all clean
 
+ifeq ($(mode), release)
+    CFLAGS += RELEASE_CFLAGS
+else
 ifeq ($(mode), debug)
-	CFLAGS += DEBUG_CFLAGS
-else ifeq ($(mode), 
+    CFLAGS += DEBUG_CFLAGS
+else
+    $(error mode was not set or set to an invalid value.)
+endif
+endif
 
 clean:
 	@echo cleaning!
