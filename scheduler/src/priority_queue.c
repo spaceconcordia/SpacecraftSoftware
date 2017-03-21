d0d86b6a7da831c5acff579bcc20092957688a21
Derek Yu <derekyu624@hotmail.com>
2017-03-21 23:10:10 +0000

    Implemented the init_priority_queue function.

--- scheduler/src/priority_queue.c
+++ scheduler/src/priority_queue.c
@@ -5,3 +5,11 @@ struct priority_queue {
   size_t capacity; //const?
   size_t size;
 };
+
+struct priority_queue init_priority_queue(size_t capacity){
+    struct priority_queue p_q;
+    p_q.array = malloc(sizeof(struct task) * capacity);
+    p_q.capacity = capacity;
+    p_q.size = 0;
+    return p_q;
+}
