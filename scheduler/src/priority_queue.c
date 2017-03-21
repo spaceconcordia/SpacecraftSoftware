04b14c8948263960f778cf4807ce00ead98879ce
Derek Yu <derekyu624@hotmail.com>
2017-03-21 23:33:41 +0000

    Implemented the top_task function.

--- scheduler/src/priority_queue.c
+++ scheduler/src/priority_queue.c
@@ -14,6 +14,14 @@ struct priority_queue init_priority_queue(size_t capacity) {
     return p_q;
 }
 
+struct task top_task(const struct priority_queue* p_q) {
+  return p_q->array[0];
+}
+
+void clear_priority_queue(struct priority_queue* p_q) {
+  p_q->size = 0;
+}
+
 void kill_priority_queue(struct priority_queue* p_q) {
   free(p_q->array);
   p_q->array = NULL;
