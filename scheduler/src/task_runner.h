c49f16e8d04f19411a9f9a3e1036a3b288f848f9
vagrant <vagrant@vagrant.vm>
2017-03-25 20:18:14 +0000

    Added the start of the task runner for the scheduler.

---
+++ scheduler/src/task_runner.h
@@ -0,0 +1,22 @@
+#ifndef SCHEDULER_TASK_RUNNER_H
+#define SCHEDULER_TASK_RUNNER_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include "task.h"
+
+//based on the type of the task, decides what action to take
+bool delegate_task(struct task* task, enum task_type type);
+
+//reboots the system
+void reboot();
+
+//gets current time from the system
+void get_time();
+
+//sends out a ping
+void ping()
+
+
+
+#endif
