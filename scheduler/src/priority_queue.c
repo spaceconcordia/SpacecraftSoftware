d17c2c19a16a06d8d3c334b27c514b26d6a9593d
Derek Yu <derekyu624@hotmail.com>
2017-03-28 00:26:55 +0000

    Added max_heapify function to priority_queue.c.

--- scheduler/src/priority_queue.c
+++ scheduler/src/priority_queue.c
@@ -1,5 +1,7 @@
 #include "priority_queue.h"
 
+void max_heapify(struct priority_queue* p_q, size_t node_index);
+
 struct priority_queue {
   struct task* array;
   size_t capacity; //const?
@@ -14,8 +16,13 @@ struct priority_queue init_priority_queue(size_t capacity) {
     return p_q;
 }
 
-struct task top_task(const struct priority_queue* p_q) {
-  return p_q->array[0];
+bool top_task(const struct priority_queue* p_q, struct task* t) {
+  if (p_q->size != 0) {
+    *t = p_q->array[0];
+    return true;
+  }
+  else
+    return false;
 }
 
 void clear_priority_queue(struct priority_queue* p_q) {
@@ -27,4 +34,26 @@ void kill_priority_queue(struct priority_queue* p_q) {
   p_q->array = NULL;
   p_q->capacity = 0;
   p_q->size = 0;
+}
+
+void max_heapify(struct priority_queue* p_q, size_t node_index) {
+  size_t left = 2 * node_index + 1;
+  size_t right = 2 * node_index + 2;
+  size_t largest;
+
+  if (left < p_q->size && !is_lower_priority(p_q->array[left], p_q->array[node_index])) {
+    largest = left;
+  }
+  else largest = node_index;
+
+  if (right < p_q->size && !is_lower_priority(p_q->array[right], p_q->array[node_index])) {
+    largest = right;
+  }
+
+  if (largest != node_index) {
+    struct task temp = p_q->array[node_index];
+    p_q->array[node_index] = p_q->array[largest];
+    p_q->array[largest] = temp;
+    max_heapify(p_q, largest);
+  }
 }
